0c3a6318c500f20a4dc7d13c6a7df79d1aa7e08f
akchaud5 <Ayushkumar.chaudhary2003@gmail.com>
2025-04-01 01:34:06 -0700

    Update Vercel configuration and URLs

--- submission.cs
+++ submission.cs
@@ -13,9 +13,9 @@ namespace ConsoleApp1
     public class Program
     {
         // These URLs will be read by the autograder, please keep the variable name un-changed and link to the correct xml/xsd files.
-        public static string xmlURL = "https://xml-hosting-puqe7r360-ayushs-projects-0e853323.vercel.app/Hotels.xml"; 
-        public static string xmlErrorURL = "https://xml-hosting-puqe7r360-ayushs-projects-0e853323.vercel.app/HotelsErrors.xml"; 
-        public static string xsdURL = "https://xml-hosting-puqe7r360-ayushs-projects-0e853323.vercel.app/Hotels.xsd"; 
+        public static string xmlURL = "https://xml-hosting-ezjjdz586-ayushs-projects-0e853323.vercel.app/Hotels.xml"; 
+        public static string xmlErrorURL = "https://xml-hosting-ezjjdz586-ayushs-projects-0e853323.vercel.app/HotelsErrors.xml"; 
+        public static string xsdURL = "https://xml-hosting-ezjjdz586-ayushs-projects-0e853323.vercel.app/Hotels.xsd"; 
 
         public static void Main(string[] args)
         {
